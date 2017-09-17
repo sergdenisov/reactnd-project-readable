716892c52bb98c463088e069081aac6fcb5fef06
Sergey Denisov <xsergeantx@mail.ru>
2017-09-17 21:26:27 +0300

    feat: Add voting ability

--- frontend/src/reducers/posts.js
+++ frontend/src/reducers/posts.js
@@ -1,9 +1,13 @@
-import { ADD_POSTS } from '../actions/posts';
+import { ADD_POSTS, EDIT_POST } from '../actions/posts';
 
 function posts(state = [], action) {
   switch (action.type) {
     case ADD_POSTS:
       return [...state, ...action.posts];
+    case EDIT_POST:
+      return state.map(
+        item => (item.id === action.post.id ? action.post : item),
+      );
     default:
       return state;
   }
